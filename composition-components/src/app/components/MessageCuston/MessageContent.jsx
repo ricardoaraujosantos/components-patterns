1661bb9067d3e069642e30ea141d21ce0d297804
RicardoCorbee <ricardo@corbee.com.br>
2024-10-20 14:36:19 -0300

    ajuste estilos

--- composition-components/src/app/components/MessageCuston/MessageContent.jsx
+++ composition-components/src/app/components/MessageCuston/MessageContent.jsx
@@ -1,7 +1,7 @@
 export const MessageContent = ({textMensage}) => {
     return(
         <div className="flex-1 flex justify-center items-center flex-col gap-2">
-            <p className="text-sm text-slate-950 mx-2">
+            <p className="text-sm text-zinc-50 mx-2">
                 {textMensage}
             </p>
         </div>
--- composition-components/src/app/components/MessageCuston/MessageRoot.jsx
+++ composition-components/src/app/components/MessageCuston/MessageRoot.jsx
@@ -1,7 +1,8 @@
 export const MessageRoot = ({ children }) => {
     return (
-        <div className="w-[364px] bg-zinc-200 px-8 py-4 flex justify-center items-center">
+        <div className="w-[364px] bg-zinc-200 rounded dark:bg-zinc-800 px-8 py-4 flex justify-center items-center">
             {children}
         </div>
+      
     )
 }
